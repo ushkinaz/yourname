9eac55e8c5c14c236b8665b2e6e5b47032c7302c
Dmitry Sidorenko <rustydragon@gmail.com>
2010-05-12 19:19:28 +0400

    First (maybe) runnable version

--- gen/com/google/code/yourname/R.java
+++ gen/com/google/code/yourname/R.java
@@ -10,14 +10,40 @@ package com.google.code.yourname;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /**  Retrieved via Resources.getColor() and friends. 
+         */
+        public static final int red=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        /**  Retrieved via Resources.getDrawable() and friends. 
+         */
+        public static final int semi_black=0x7f020001;
+    }
+    public static final class id {
+        public static final int calculate=0x7f070004;
+        public static final int editor=0x7f070000;
+        public static final int modality=0x7f070003;
+        public static final int number=0x7f070002;
+        public static final int sum=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**  Simple strings. 
+         */
+        public static final int app_name=0x7f050000;
+        public static final int calculate=0x7f050001;
+        public static final int clear=0x7f050002;
+        /**  This is a complex string containing style runs. 
+         */
+        public static final int main_label=0x7f050003;
+    }
+    public static final class style {
+        public static final int ActionButton=0x7f060000;
+        public static final int TextAppearance=0x7f060001;
+        public static final int TextAppearance_ActionButton=0x7f060002;
     }
 }
--- src/com/google/code/yourname/MainActivity.java
+++ src/com/google/code/yourname/MainActivity.java
@@ -2,18 +2,82 @@ package com.google.code.yourname;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.method.BaseKeyListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.code.yourname.numerology.NumerologyCalculator;
+import com.google.code.yourname.numerology.NumerologyCalculatorRussian;
+import com.google.code.yourname.numerology.NumerologyResult;
 
 /**
  * Date: 11.05.2010
  * Created by Dmitry Sidorenko.
  */
 public class MainActivity extends Activity {
+
+    private NumerologyCalculator numerologyCalculator;
+    private EditText mEditor;
+    private TextView sum;
+    private TextView number;
+    private TextView modality;
+
+    public MainActivity() {
+        numerologyCalculator = new NumerologyCalculatorRussian();
+    }
+
     /**
-     * Called when the activity is first created.
+     * Called with the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Inflate our UI from its XML layout description.
         setContentView(R.layout.main);
+
+        // Find the text editor view inside the layout, because we
+        // want to do various programmatic things with it.
+        mEditor = (EditText) findViewById(R.id.editor);
+
+        sum = (EditText) findViewById(R.id.sum);
+        number = (EditText) findViewById(R.id.number);
+        modality = (EditText) findViewById(R.id.modality);
+
+        // Hook up button presses to the appropriate event handler.
+        findViewById(R.id.calculate).setOnClickListener(mBackListener);
+
+        mEditor.setText(getText(R.string.main_label));
+        mEditor.setKeyListener(new BaseKeyListener() {
+            public int getInputType() {
+                return InputType.TYPE_TEXT_VARIATION_PERSON_NAME;
+            }
+
+            @Override
+            public boolean onKeyDown(View view, Editable content, int keyCode, KeyEvent event) {
+                nameUpdated();
+                return super.onKeyDown(view, content, keyCode, event);
+            }
+        });
     }
+
+    private void nameUpdated() {
+        NumerologyResult result = numerologyCalculator.calculateResult(mEditor.getText().toString());
+        sum.setText(Integer.toString(result.getSum()));
+        number.setText(Integer.toString(result.getNumber()));
+        modality.setText(Integer.toString(result.getModality()));
+    }
+
+    /**
+     * A call-back for when the user presses the calculate button.
+     */
+    View.OnClickListener mBackListener = new View.OnClickListener() {
+        public void onClick(View v) {
+
+        }
+    };
+
 }
